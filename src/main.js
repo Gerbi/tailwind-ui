4a3e1953eb0ef8d15a5fefe73719239fc430afa4
Gerbi <gerbi@users.noreply.github.com>
2020-02-05 02:26:45 -0600

    Fix Profile Link

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
-// import '@lottiefiles/lottie-player';
+import '@lottiefiles/lottie-player';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
 Vue.config.productionTip = false;
