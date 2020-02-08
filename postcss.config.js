274744a444454f0f95f9a84b3965cfb4af386bee
Gerbi <gerbi@users.noreply.github.com>
2020-02-07 23:33:08 -0600

    Install purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,22 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const purgeCSS = require('@fullhuman/postcss-purgecss')({
+
+  content: [
+    './src/**/*.html',
+    './src/**/*.vue',
+    './src/**/*.jsx',
+  ],
+  defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+});
+
 module.exports = {
-    plugins: [
-        require('tailwindcss'),
-        require('autoprefixer'),
-    ]
-}
+  plugins: [
+    // eslint-disable-next-line import/no-extraneous-dependencies,global-require
+    require('tailwindcss'),
+    // eslint-disable-next-line import/no-extraneous-dependencies,global-require
+    require('autoprefixer'),
+    ...process.env.NODE_ENV === 'production'
+      ? [purgeCSS]
+      : [],
+  ],
+};
