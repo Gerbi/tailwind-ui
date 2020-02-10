2ea9033a54ce027b471208b3570c491cc9c4c36d
Gerbi <gerbi@users.noreply.github.com>
2020-02-09 23:04:11 -0600

    Fix

--- src/components/vk/sidebar.vue
+++ src/components/vk/sidebar.vue
@@ -8,79 +8,77 @@
         </svg>
         <span class="ml-4 text-gray-800">News</span>
       </span>
-      <span class="inline-block w-9 text-center py-1 leading-none text-xs font-semibold text-gray-700 bg-gray-300 rounded-full">6</span>
+      <span class="inline-block w-9 text-center py-1 leading-none text-xs font-semibold text-gray-700 bg-gray-300 rounded-full">12</span>
     </a>
     <a href="#" class="mt-1 -mx-3 px-3 py-2 flex items-center justify-between text-sm font-medium hover:bg-gray-200 rounded-lg">
-                <span class="inline-flex items-center">
-                  <svg viewBox="0 0 24 24" class="h-7 w-7 fill-current text-blue-600">
-                      <path d="M12 22a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16zM9.56 8.93l6.37-2.12a1 1 0 0 1 1.26 1.26l-2.12 6.37a1 1 0 0 1-.63.63l-6.37 2.12a1 1 0 0 1-1.26-1.26l2.12-6.37a1 1 0 0 1 .63-.63zm-.22 5.73l4-1.33 1.32-4-4 1.34-1.32 4z"/>
-                  </svg>
-                  <span class="ml-4 text-gray-800">Recommended</span>
-                </span>
+      <span class="inline-flex items-center">
+        <svg viewBox="0 0 24 24" class="h-7 w-7 fill-current text-blue-600">
+            <path d="M12 22a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16zM9.56 8.93l6.37-2.12a1 1 0 0 1 1.26 1.26l-2.12 6.37a1 1 0 0 1-.63.63l-6.37 2.12a1 1 0 0 1-1.26-1.26l2.12-6.37a1 1 0 0 1 .63-.63zm-.22 5.73l4-1.33 1.32-4-4 1.34-1.32 4z"/>
+        </svg>
+        <span class="ml-4 text-gray-800">Recommended</span>
+      </span>
       <span class="inline-block w-9 text-center py-1 leading-none text-xs font-semibold text-gray-700 bg-gray-300 rounded-full">25</span>
     </a>
     <a href="#" class="mt-1 -mx-3 px-3 py-2 flex items-center justify-between text-sm font-medium hover:bg-gray-200 rounded-lg">
-                <span class="inline-flex items-center">
-                  <svg viewBox="0 0 24 24" class="h-7 w-7 fill-current text-blue-600">
-                      <path d="M15 19a3 3 0 0 1-6 0H4a1 1 0 0 1 0-2h1v-6a7 7 0 0 1 4.02-6.34 3 3 0 0 1 5.96 0A7 7 0 0 1 19 11v6h1a1 1 0 0 1 0 2h-5zm-4 0a1 1 0 0 0 2 0h-2zm0-12.9A5 5 0 0 0 7 11v6h10v-6a5 5 0 0 0-4-4.9V5a1 1 0 0 0-2 0v1.1z"/>
-                  </svg>
-                  <span class="ml-4 text-gray-800">Notifications</span>
-                </span>
+      <span class="inline-flex items-center">
+        <svg viewBox="0 0 24 24" class="h-7 w-7 fill-current text-blue-600">
+            <path d="M15 19a3 3 0 0 1-6 0H4a1 1 0 0 1 0-2h1v-6a7 7 0 0 1 4.02-6.34 3 3 0 0 1 5.96 0A7 7 0 0 1 19 11v6h1a1 1 0 0 1 0 2h-5zm-4 0a1 1 0 0 0 2 0h-2zm0-12.9A5 5 0 0 0 7 11v6h10v-6a5 5 0 0 0-4-4.9V5a1 1 0 0 0-2 0v1.1z"/>
+        </svg>
+        <span class="ml-4 text-gray-800">Notifications</span>
+      </span>
       <span class="inline-block w-9 text-center py-1 leading-none text-xs font-semibold text-gray-700 bg-gray-300 rounded-full">6</span>
     </a>
     <a href="#" class="mt-1 -mx-3 px-3 py-2 flex items-center justify-between text-sm font-medium hover:bg-gray-200 rounded-lg">
-                <span class="inline-flex items-center">
-                  <svg viewBox="0 0 24 24" class="h-7 w-7 fill-current text-blue-500">
-                    <path d="M2 15V5c0-1.1.9-2 2-2h16a2 2 0 0 1 2 2v15a1 1 0 0 1-1.7.7L16.58 17H4a2 2 0 0 1-2-2zM20 5H4v10h13a1 1 0 0 1 .7.3l2.3 2.29V5z"/>
-                  </svg>
-                  <span class="ml-4 text-gray-800">Messages</span>
-                </span>
-      <span
-        class="inline-block w-9 text-center py-1 leading-none text-xs font-semibold text-gray-700 bg-gray-300 rounded-full">
-                  1
-                </span>
+      <span class="inline-flex items-center">
+        <svg viewBox="0 0 24 24" class="h-7 w-7 fill-current text-blue-500">
+          <path d="M2 15V5c0-1.1.9-2 2-2h16a2 2 0 0 1 2 2v15a1 1 0 0 1-1.7.7L16.58 17H4a2 2 0 0 1-2-2zM20 5H4v10h13a1 1 0 0 1 .7.3l2.3 2.29V5z"/>
+        </svg>
+        <span class="ml-4 text-gray-800">Messages</span>
+      </span>
+      <span class="inline-block w-9 text-center py-1 leading-none text-xs font-semibold text-gray-700 bg-gray-300 rounded-full">1
+      </span>
     </a>
     <a href="#" class="mt-1 -mx-3 px-3 py-2 flex items-center justify-between text-sm font-medium hover:bg-gray-200 rounded-lg">
-                <span class="inline-flex items-center">
-                  <svg viewBox="0 0 24 24" class="h-7 w-7 fill-current text-blue-500">
-                    <path d="M12 12a5 5 0 1 1 0-10 5 5 0 0 1 0 10zm0-2a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm9 11a1 1 0 0 1-2 0v-2a3 3 0 0 0-3-3H8a3 3 0 0 0-3 3v2a1 1 0 0 1-2 0v-2a5 5 0 0 1 5-5h8a5 5 0 0 1 5 5v2z"/>
-                  </svg>
-                  <span class="ml-4 text-gray-800">Friends</span>
-                </span>
+      <span class="inline-flex items-center">
+        <svg viewBox="0 0 24 24" class="h-7 w-7 fill-current text-blue-500">
+          <path d="M12 12a5 5 0 1 1 0-10 5 5 0 0 1 0 10zm0-2a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm9 11a1 1 0 0 1-2 0v-2a3 3 0 0 0-3-3H8a3 3 0 0 0-3 3v2a1 1 0 0 1-2 0v-2a5 5 0 0 1 5-5h8a5 5 0 0 1 5 5v2z"/>
+        </svg>
+        <span class="ml-4 text-gray-800">Friends</span>
+      </span>
     </a>
     <a href="#" class="mt-1 -mx-3 px-3 py-2 flex items-center justify-between text-sm font-medium hover:bg-gray-200 rounded-lg">
-                <span class="inline-flex items-center">
-                  <svg viewBox="0 0 24 24" class="h-7 w-7 fill-current text-blue-500">
-                    <path d="M9 12A5 5 0 1 1 9 2a5 5 0 0 1 0 10zm0-2a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm7 11a1 1 0 0 1-2 0v-2a3 3 0 0 0-3-3H7a3 3 0 0 0-3 3v2a1 1 0 0 1-2 0v-2a5 5 0 0 1 5-5h4a5 5 0 0 1 5 5v2zm1-5a1 1 0 0 1 0-2 5 5 0 0 1 5 5v2a1 1 0 0 1-2 0v-2a3 3 0 0 0-3-3zm-2-4a1 1 0 0 1 0-2 3 3 0 0 0 0-6 1 1 0 0 1 0-2 5 5 0 0 1 0 10z"/>
-                  </svg>
-                  <span class="ml-4 text-gray-800">Communities</span>
-                </span>
+      <span class="inline-flex items-center">
+        <svg viewBox="0 0 24 24" class="h-7 w-7 fill-current text-blue-500">
+          <path d="M9 12A5 5 0 1 1 9 2a5 5 0 0 1 0 10zm0-2a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm7 11a1 1 0 0 1-2 0v-2a3 3 0 0 0-3-3H7a3 3 0 0 0-3 3v2a1 1 0 0 1-2 0v-2a5 5 0 0 1 5-5h4a5 5 0 0 1 5 5v2zm1-5a1 1 0 0 1 0-2 5 5 0 0 1 5 5v2a1 1 0 0 1-2 0v-2a3 3 0 0 0-3-3zm-2-4a1 1 0 0 1 0-2 3 3 0 0 0 0-6 1 1 0 0 1 0-2 5 5 0 0 1 0 10z"/>
+        </svg>
+        <span class="ml-4 text-gray-800">Communities</span>
+      </span>
     </a>
-    <a href="#" class="mt-1 -mx-3 px-3 py-2 flex justify-between text-sm font-medium hover:bg-gray-200 rounded-lg">
-                <span class="inline-flex items-center">
-                  <svg viewBox="0 0 24 24" class="h-7 w-7 fill-current text-blue-500">
-                    <path d="M5 3h4a2 2 0 0 1 2 2v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5c0-1.1.9-2 2-2zm0 2v4h4V5H5zm10-2h4a2 2 0 0 1 2 2v4a2 2 0 0 1-2 2h-4a2 2 0 0 1-2-2V5c0-1.1.9-2 2-2zm0 2v4h4V5h-4zM5 13h4a2 2 0 0 1 2 2v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4c0-1.1.9-2 2-2zm0 2v4h4v-4H5zm10-2h4a2 2 0 0 1 2 2v4a2 2 0 0 1-2 2h-4a2 2 0 0 1-2-2v-4c0-1.1.9-2 2-2zm0 2v4h4v-4h-4z"/>
-                  </svg>
-                  <span class="ml-4 text-gray-800">Media</span>
-                </span>
+    <a href="#" class="mt-1 -mx-3 px-3 py-2 flex items-center justify-between text-sm font-medium hover:bg-gray-200 rounded-lg">
+      <span class="inline-flex items-center">
+        <svg viewBox="0 0 24 24" class="h-7 w-7 fill-current text-blue-500">
+          <path d="M5 3h4a2 2 0 0 1 2 2v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5c0-1.1.9-2 2-2zm0 2v4h4V5H5zm10-2h4a2 2 0 0 1 2 2v4a2 2 0 0 1-2 2h-4a2 2 0 0 1-2-2V5c0-1.1.9-2 2-2zm0 2v4h4V5h-4zM5 13h4a2 2 0 0 1 2 2v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4c0-1.1.9-2 2-2zm0 2v4h4v-4H5zm10-2h4a2 2 0 0 1 2 2v4a2 2 0 0 1-2 2h-4a2 2 0 0 1-2-2v-4c0-1.1.9-2 2-2zm0 2v4h4v-4h-4z"/>
+        </svg>
+        <span class="ml-4 text-gray-800">Media</span>
+      </span>
     </a>
-    <a href="#" class="mt-1 -mx-3 px-3 py-2 flex justify-between text-sm font-medium hover:bg-gray-200 rounded-lg">
-                <span class="inline-flex items-center">
-                  <svg viewBox="0 0 24 24" class="h-7 w-7 fill-current text-blue-500">
-                    <path d="M6.1 21.98a1 1 0 0 1-1.45-1.06l1.03-6.03-4.38-4.26a1 1 0 0 1 .56-1.71l6.05-.88 2.7-5.48a1 1 0 0 1 1.8 0l2.7 5.48 6.06.88a1 1 0 0 1 .55 1.7l-4.38 4.27 1.04 6.03a1 1 0 0 1-1.46 1.06l-5.4-2.85-5.42 2.85zm4.95-4.87a1 1 0 0 1 .93 0l4.08 2.15-.78-4.55a1 1 0 0 1 .29-.88l3.3-3.22-4.56-.67a1 1 0 0 1-.76-.54l-2.04-4.14L9.47 9.4a1 1 0 0 1-.75.54l-4.57.67 3.3 3.22a1 1 0 0 1 .3.88l-.79 4.55 4.09-2.15z"/>
-                  </svg>
-                  <span class="ml-4 text-gray-800">Bookmarks</span>
-                </span>
+    <a href="#" class="mt-1 -mx-3 px-3 py-2 flex items-center justify-between text-sm font-medium hover:bg-gray-200 rounded-lg">
+      <span class="inline-flex items-center">
+        <svg viewBox="0 0 24 24" class="h-7 w-7 fill-current text-blue-500">
+          <path d="M6.1 21.98a1 1 0 0 1-1.45-1.06l1.03-6.03-4.38-4.26a1 1 0 0 1 .56-1.71l6.05-.88 2.7-5.48a1 1 0 0 1 1.8 0l2.7 5.48 6.06.88a1 1 0 0 1 .55 1.7l-4.38 4.27 1.04 6.03a1 1 0 0 1-1.46 1.06l-5.4-2.85-5.42 2.85zm4.95-4.87a1 1 0 0 1 .93 0l4.08 2.15-.78-4.55a1 1 0 0 1 .29-.88l3.3-3.22-4.56-.67a1 1 0 0 1-.76-.54l-2.04-4.14L9.47 9.4a1 1 0 0 1-.75.54l-4.57.67 3.3 3.22a1 1 0 0 1 .3.88l-.79 4.55 4.09-2.15z"/>
+        </svg>
+        <span class="ml-4 text-gray-800">Bookmarks</span>
+      </span>
     </a>
-    <a href="#" class="mt-1 -mx-3 px-3 py-2 flex justify-between text-sm font-medium hover:bg-gray-200 rounded-lg">
-                <span class="inline-flex items-center">
-                  <svg viewBox="0 0 24 24" class="h-7 w-7 fill-current text-blue-500">
-                    <path d="M6 2h9a1 1 0 0 1 .7.3l4 4a1 1 0 0 1 .3.7v13a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V4c0-1.1.9-2 2-2zm9 2.41V7h2.59L15 4.41zM18 9h-3a2 2 0 0 1-2-2V4H6v16h12V9z"/>
-                  </svg>
-                  <span class="ml-4 text-gray-800">Documents</span>
-                </span>
+    <a href="#" class="mt-1 -mx-3 px-3 py-2 flex items-center justify-between text-sm font-medium hover:bg-gray-200 rounded-lg">
+      <span class="inline-flex items-center">
+        <svg viewBox="0 0 24 24" class="h-7 w-7 fill-current text-blue-500">
+          <path d="M6 2h9a1 1 0 0 1 .7.3l4 4a1 1 0 0 1 .3.7v13a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V4c0-1.1.9-2 2-2zm9 2.41V7h2.59L15 4.41zM18 9h-3a2 2 0 0 1-2-2V4H6v16h12V9z"/>
+        </svg>
+        <span class="ml-4 text-gray-800">Documents</span>
+      </span>
     </a>
-    <a href="#" class="mt-1 -mx-3 px-3 py-2 flex justify-between text-sm font-medium hover:bg-gray-200 rounded-lg">
+    <a href="#" class="mt-1 -mx-3 px-3 py-2 flex items-center justify-between text-sm font-medium hover:bg-gray-200 rounded-lg">
       <span class="inline-flex items-center">
         <svg viewBox="0 0 24 24" class="h-7 w-7 fill-current text-blue-500">
           <path d="M12.76 3.76a6 6 0 0 1 8.48 8.48l-8.53 8.54a1 1 0 0 1-1.42 0l-8.53-8.54a6 6 0 0 1 8.48-8.48l.76.75.76-.75zm7.07 7.07a4 4 0 1 0-5.66-5.66l-1.46 1.47a1 1 0 0 1-1.42 0L9.83 5.17a4 4 0 1 0-5.66 5.66L12 18.66l7.83-7.83z"/>
