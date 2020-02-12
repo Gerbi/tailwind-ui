7be7f453a883270d472cf1b8cbdc1fe7be99e1f5
Gerbi <gerbi@users.noreply.github.com>
2020-02-11 20:49:34 -0600

    Fix

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,5 @@
 <template>
-  <div id="app" class="font-sans">
-    <!-- eslint-disable max-len -->
-<!--    <div class="absolute z-50 flex w-full justify-center mx-auto mt-4">-->
-<!--      <div class="flex bg-gray-800 text-white rounded-lg py-3 shadow-lg">-->
-<!--        <div class="mx-4">-->
-<!--          <router-link to="/">Home</router-link>-->
-<!--        </div>-->
-<!--        <div class="mx-4">-->
-<!--          <router-link to="/layout">Layout</router-link>-->
-<!--        </div>-->
-<!--        <div class="mx-4">-->
-<!--          <router-link to="/schedule">Schedule</router-link>-->
-<!--        </div>-->
-<!--        <div class="mx-4">-->
-<!--          <router-link to="/kod">Kod</router-link>-->
-<!--        </div>-->
-<!--        <div class="mx-4">-->
-<!--          <router-link to="/rnts">Rnts</router-link>-->
-<!--        </div>-->
-<!--        <div class="mx-4">-->
-<!--          <router-link to="/news">News</router-link>-->
-<!--        </div>-->
-<!--        <div class="mx-4">-->
-<!--          <router-link to="/pages">Pages</router-link>-->
-<!--        </div>-->
-<!--        <div class="mx-4">-->
-<!--          <router-link to="/test">Test</router-link>-->
-<!--        </div>-->
-<!--      </div>-->
-<!--    </div>-->
+  <div id="app">
     <router-view></router-view>
   </div>
 </template>
